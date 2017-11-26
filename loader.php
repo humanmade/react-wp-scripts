adc5504f35012262fd5d1a8249f194bc2ab81004
K. Adam White <kadamwhite@users.noreply.github.com>
2017-11-25 23:31:15 -0500

    Use webpack-manifest-plugin to read and auto-enqueue all CSS & JS

--- loader.php
+++ loader.php
@@ -15,45 +15,121 @@ function is_development() {
 }
 
 /**
- * Get the port for React's development server.
+ * Create a probably-unique key for a given script bundle.
  *
- * @return int|null Port number if available, otherwise null.
+ * @param string $path A relative filesystem path or resource URI.
+ * @return string
  */
-function get_react_port() {
-	if ( ! is_development() ) {
-		return null;
-	}
-
-	$path = get_theme_file_path( 'react-port' );
-	if ( ! file_exists( $path ) ) {
-		return null;
-	}
-
-	$port = file_get_contents( $path );
-	if ( empty( $port ) || ! is_numeric( $port ) ) {
-		return null;
-	}
-
-	return (int) trim( $port );
+function get_asset_key( string $path ) {
+	$path_parts = explode( '/', $path );
+	return 'react_wp_script_' . end( $path_parts );
 }
 
-function enqueue_assets( $id, $deps = [] ) {
-	$port = get_react_port();
-	if ( $port ) {
-		wp_enqueue_script(
-			$id,
-			sprintf( 'http://localhost:%d/static/js/bundle.js', $port ),
-			$deps,
-			null,
-			true
-		);
-	} else {
-		wp_enqueue_script(
-			$id,
-			get_theme_file_uri( 'build/js/main.js' ),
-			$deps,
-			null,
-			true
-		);
+/**
+ * Convert a relative path within a theme or plugin to an absolute filesystem path.
+ *
+ * @param string $path     A relative file system path.
+ * @param bool   $is_theme Whether to treat the $path as a theme file.
+ * @return string;
+ */
+function get_file_path( string $path, bool $is_theme ) {
+	return $is_theme
+		? get_theme_file_path( $path )
+		: plugin_dir_path( __file__ ) . $path;
+}
+
+/**
+ * Check the filesystem for asset manifests for a theme or plugin and attempt to
+ * decode and return the asset list JSON if found.
+ *
+ * @param bool $is_theme Whether to look for the manifest in a theme or plugin context.
+ * @return array|null;
+ */
+function get_assets_list( string $is_theme ) {
+	if ( is_development() ) {
+		$dev_assets_path = get_file_path( 'asset-manifest.json', $is_theme );
+		if ( file_exists( $dev_assets_path ) ) {
+			$build_assets_contents = file_get_contents( $dev_assets_path );
+			if ( ! empty( $build_assets_contents ) ) {
+				return json_decode( file_get_contents( $dev_assets_path ), true );
+			}
+		}
 	}
+	$build_assets_path = get_file_path( 'build/asset-manifest.json', $is_theme );
+	if ( file_exists( $build_assets_path ) ) {
+		$build_assets_contents = file_get_contents( $build_assets_path );
+		if ( ! empty( $build_assets_contents ) ) {
+			return json_decode( file_get_contents( $build_assets_path ), true );
+		}
+	}
+	return null;
+}
+
+
+/**
+ * Return web URIs or convert relative filesystem paths to absolute paths.
+ *
+ * @param string $asset_path  A relative filesystem path or resource URI.
+ * @param bool   $is_theme    Whether to treat the $path as a theme file.
+ * @return string
+ */
+function get_asset_uri( string $asset_path, bool $is_theme ) {
+	if ( strpos( $asset_path, '://' ) !== false ) {
+		return $asset_path;
+	}
+	return $is_theme
+		? get_theme_file_uri( $asset_path )
+		: plugin_dir_url( __file__ ) . $asset_path;
+}
+
+/**
+ * Search for webpack-manifest-plugin output, attempt to load it, and enqueue
+ * any scripts or styles contained within.
+ *
+ * @param bool $is_theme Whether to look for files in a theme or plugin context.
+ */
+function autoenqueue_assets( bool $is_theme ) {
+	// Attempt to load
+	$assets = get_assets_list( $is_theme );
+	if ( empty( $assets ) ) {
+		// TODO: needs error condition.
+		return;
+	}
+
+	foreach ( $assets as $key => $asset_path ) {
+		$is_js = preg_match( '/\.js$/', $asset_path );
+		$is_css = preg_match( '/\.css$/', $asset_path );
+		if ( ! $is_js && ! $is_css ) {
+			continue;
+		}
+		// Asset file contains URIs, enqueue them directly.
+		if ( $is_js ) {
+			wp_enqueue_script(
+				get_asset_key( $asset_path ),
+				get_asset_uri( $asset_path, $is_theme ),
+				[],
+				null,
+				true
+			);
+		} else if ( $is_css ) {
+			wp_enqueue_style(
+				get_asset_key( $asset_path ),
+				get_asset_uri( $asset_path, $is_theme )
+			);
+		}
+	}
+}
+
+/**
+ * Wrapper function to auto-enqueue all built assets for a theme.
+ */
+function autoenqueue_theme_assets() {
+	autoenqueue_assets( true );
+}
+
+/**
+ * Wrapper function to auto-enqueue all built assets for a plugin.
+ */
+function autoenqueue_plugin_assets() {
+	autoenqueue_assets( false );
 }
--- scripts/start.js
+++ scripts/start.js
@@ -3,22 +3,24 @@ const { spawn } = require( 'child_process' );
 const fs = require( 'fs' );
 const path = require( 'path' );
 const onExit = require( 'signal-exit' );
+const overrideConfigCache = require( './override-start' );
 
 const {
-  choosePort,
-  createCompiler,
-  prepareProxy,
-  prepareUrls,
+	choosePort,
+	createCompiler,
+	prepareProxy,
+	prepareUrls,
 } = require('react-dev-utils/WebpackDevServerUtils');
 
 const DEFAULT_PORT = parseInt(process.env.PORT, 10) || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
-const PORT_FILE = path.join( process.cwd(), 'react-port' );
+const ASSET_FILENAME = 'asset-manifest.json'
+const ASSET_FILE_PATH = path.join( process.cwd(), ASSET_FILENAME );
 
-fs.readFile( PORT_FILE, ( err, data ) => {
+fs.readFile( ASSET_FILE_PATH, ( err, data ) => {
 	if ( ! err ) {
-		console.log( chalk.green( `Already running on port ${ data }` ) );
+		console.log( chalk.green( `${ ASSET_FILENAME } found: assuming dev server is already running.` ) );
 		return;
 	}
 
@@ -27,30 +29,18 @@ fs.readFile( PORT_FILE, ( err, data ) => {
 			// We have not found a port.
 			return;
 		}
+		const protocol = process.env.HTTPS === 'true' ? 'https' : 'http';
 
-		try {
-			fs.writeFileSync( PORT_FILE, port );
-		} catch ( err ) {
-			console.log( chalk.red( `Unable to write port to ${ PORT_FILE }` ) );
-			return;
-		}
+		// Pass in the full hostname of the dev server.
+		overrideConfigCache( `${ protocol }://${ HOST }:${ port }` );
 
-		const overridePath = require.resolve( './override-start' );
-		const env = {
-			PORT:    port,
-			BROWSER: 'none',
-		};
-		const opts = {
-			env:   Object.assign( {}, process.env, env ),
-			stdio: 'inherit',
-		};
-		const proc = spawn(
-			process.execPath,
-			[ overridePath ],
-			opts
-		);
+		// Pass the selected port forward so that react-scripts' start will not re-prompt.
+		process.env.PORT = port;
+
+		// Load the parent start script now that configuration patching is complete.
+		require( 'react-scripts/scripts/start' );
 
 		// Before exit, delete the port file.
-		onExit( code => fs.unlinkSync( PORT_FILE ) );
+		onExit( () => fs.unlinkSync( ASSET_FILE_PATH ) );
 	} );
 } );
