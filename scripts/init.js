76e49e05cd6a9363426889c5f2e7f869f05d617e
Ignacio Cruz <igmoweb@gmail.com>
2017-12-05 13:29:45 +0100

    Setup rest of npm scripts

--- scripts/init.js
+++ scripts/init.js
@@ -23,6 +23,9 @@ module.exports = function(
 	// Setup the script rules
 	appPackage.scripts = {
 		start: 'react-wp-scripts start',
+		build: 'react-scripts build',
+		test: 'react-scripts test --env=jsdom',
+		eject: 'react-scripts eject',
 	};
 
 	fs.writeFileSync(
