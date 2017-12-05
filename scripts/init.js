3abbdbee119db3228d90a5bd23d60ec8ebf0eb44
Ignacio Cruz <igmoweb@gmail.com>
2017-12-05 13:01:58 +0100

    Debugging init.js

--- scripts/init.js
+++ scripts/init.js
@@ -1,1 +1,22 @@
-console.log( "THIS IS A TEST" );
+'use strict';
+
+module.exports = function(
+	appPath,
+	appName,
+	verbose,
+	originalDirectory,
+	template
+) {
+	console.log("appPath");
+	console.log( appPath );
+	console.log("appName");
+	console.log( appName );
+	console.log("verbose");
+	console.log( verbose );
+	console.log("originalDirectory");
+	console.log( originalDirectory );
+	console.log("template");
+	console.log( template );
+	return;
+
+};
