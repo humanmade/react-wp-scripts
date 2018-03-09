8a1bc2a84afe68392d54dd53584dd6ca8f47af41
Ryan McCue <me@ryanmccue.info>
2018-03-09 17:51:59 +1000

    Add minimum viable theme files

---
+++ packages/react-wp-scripts/template/theme/header.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Base header for the theme.
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes() ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ) ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
